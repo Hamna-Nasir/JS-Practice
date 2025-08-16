b1f92d3bcc2d5d1b8e08395a90b864340d5a5aa8
Hamna-Nasir <hamnanasir.10r@gmail.com>
2025-08-16 17:57:29 +0500

    Math & Number

---
+++ 01-Basics/06-numMaths.js
@@ -0,0 +1,31 @@
+// Numbers
+const balance = 1000;
+//console.log(balance);
+
+const balance1 = new Number(2000);
+//console.log(balance1);
+
+// console.log(balance1.toString().length);
+// console.log(balance1.toFixed(2));
+
+const otherNum = 123.56;
+// console.log(otherNum.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10 ;
+const max = 20 ;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
