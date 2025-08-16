9ca5b8853e10388c4328f6968e882b9549279a88
Hamna-Nasir <hamnanasir.10r@gmail.com>
2025-08-16 14:31:17 +0500

    comparisions.js

---
+++ 01-Basics/04-comparision.js
@@ -0,0 +1,22 @@
+console.log(10 == 10); // true
+console.log(10 == "10"); // true
+console.log(10 === "10"); // false
+console.log(10 != 10); // false
+console.log(10 != "10"); // false
+console.log(10 !== "10"); // true
+console.log(10 > 10); // false
+console.log(10 < 10); // false
+console.log(10 >= 10); // true
+console.log(10 <= 10);
+console.log(2>=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null >= 0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined < 0);
