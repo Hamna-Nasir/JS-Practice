fddf45410cae06ab8f0c9ba61515056b932bb277
Hamna-Nasir <hamnanasir.10r@gmail.com>
2025-08-17 12:13:15 +0500

    Updated files

---
+++ 02-Basics/02-Arrays.js
@@ -0,0 +1,27 @@
+const marvelHeros = ["thor", "Ironman", "spiderman"];
+const dcHeros = ["superman", "flash", "batman"];
+
+// marvelHeros.push(dcHeros); 
+
+// console.log(marvelHeros);
+// console.log(marvelHeros[3][1]);
+
+// const allHeros = marvelHeros.concat(dcHeros);
+// console.log(allHeros);
+
+const allNewHeros = [...marvelHeros, ...dcHeros];
+// console.log(allNewHeros);
+
+const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+const anotherArray1 = anotherArray.flat(Infinity);
+// console.log(anotherArray1);
+
+console.log(Array.isArray("Hamna"));
+console.log(Array.from("Hamna"));
+console.log(Array.from({ name: "Hamna" })); 
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
