839ed59aacbca98231eba2eed0a5f7fa50c93379
Hamna-Nasir <hamnanasir.10r@gmail.com>
2025-08-27 15:19:52 +0500

    updated files

---
+++ 07-Projects/p2-BMICalculator/main1.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+// const height = parseInt(document.querySelector('#height').value);
+// this usecase will give us empty value
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    if ((height === "") || (height < 0) || isNaN(height)) {
+      results.innerHTML = `Please give valid height ${height}`;
+    }else if ((weight === "") || (weight < 0) || isNaN(weight)) {
+      results.innerHTML = `Please give valid weight ${weight}`;
+    }else{
+      const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+      // showing the results
+      results.innerHTML = `<span>${bmi}</span> `;
+    }
+})
